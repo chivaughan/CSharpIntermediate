bb7ab0a2801a7ceba0986719be128f16c20f0cad
Chivaughan <chivaughan007@gmail.com>
2023-01-22 00:03:19 +0100

    Add project files.

---
+++ CSharpIntermediate/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+
+namespace CSharpIntermediate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopWatch = new StopWatch();
+            stopWatch.Start();
+            stopWatch.Start();
+            stopWatch.Stop();
+            Console.WriteLine(stopWatch.Duration);            
+        }
+    }
+}
---
+++ CSharpIntermediate/StopWatch.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpIntermediate
+{
+    public class StopWatch
+    {
+        public TimeSpan Duration 
+        { 
+            get
+            {
+                return EndTime - StartTime;
+            }
+            set { }
+        }
+        public TimeSpan StartTime { get; set; }
+        public TimeSpan EndTime { get; set; }
+
+        int ActiveInstances { get; set; }
+        public StopWatch()
+        {
+            ActiveInstances = 0;
+        }
+
+        public void Start()
+        {
+            if (ActiveInstances >= 1)
+                throw new InvalidOperationException("You have already started the stop watch");
+            StartTime = DateTime.Now.TimeOfDay;
+            Console.WriteLine(StartTime);
+            Console.WriteLine("StopWatch has Started");
+            ActiveInstances += 1;
+        }
+
+        public void Stop()
+        {
+            ActiveInstances -= 1;
+            EndTime = DateTime.Now.TimeOfDay;
+            Console.WriteLine(EndTime);
+            Console.WriteLine("StopWatch has Stopped");
+        }
+    }
+}
